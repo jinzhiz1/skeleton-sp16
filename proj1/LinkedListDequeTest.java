dc037c8dd3e641d82370d98fac8baba0a28d4ed4
jinzhiz1 <jinzhiz1@andrew.cmu.edu>
2017-07-05 14:13:59 +0800

    make up the LinkedListTest

--- proj1/LinkedListDequeTest.java
+++ proj1/LinkedListDequeTest.java
@@ -77,8 +77,17 @@ public class LinkedListDequeTest {
 		// should be empty 
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
+		//printTestStatus(passed);
+
+		lld1.addLast(10);
+		// should not be empty 
+		passed = checkEmpty(false, lld1.isEmpty()) && passed;
+
+		lld1.removeLast();
+		// should be empty 
+		passed = checkEmpty(true, lld1.isEmpty()) && passed;
+
 		printTestStatus(passed);
-		
 	}
 
 	public static void main(String[] args) {
